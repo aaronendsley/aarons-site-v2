901097677fc956418cc18928fe047de7dc2c377b
Aaron Endsley <aaronendsley@gmail.com>
2017-07-13 17:47:42 -0500

    fixed a centering problem with css

--- src/home/home.js
+++ src/home/home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="homeBody">
       <h1>This is the beginning</h1>
-      <div className="buttonGit">
+      <div id="buttonGit">
         <Link to="/github">
           <button type="button">I do things</button>
         </Link>
