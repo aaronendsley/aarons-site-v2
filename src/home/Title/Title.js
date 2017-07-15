01eb2e3ae241b7d41e9569a5d0f3400e5107501a
Aaron Endsley <aaronendsley@gmail.com>
2017-07-15 11:16:55 -0500

    improving the title component

--- src/home/Title/Title.js
+++ src/home/Title/Title.js
@@ -2,21 +2,15 @@ import React from 'react';
 import './Title.css';
 
 const Title = () => {
+  const title = ['let person = {', '"Name": "Aaron Endsley",', '"Description" : "Front End Web Dev"', '}'];
   return (
     <div className="Title">
       <div className="innerTitleDiv">
-        <h1>
-          {'const person = {'}
-        </h1>
-        <h1>
-          {' "Name": "Aaron Endsley", '}
-        </h1>
-        <h1>
-          {' "Description" : "Front End Web Dev"'}
-        </h1>
-        <h1>
-          {'}'}
-        </h1>
+        {title.map(item =>
+          <h1>
+            {item}
+          </h1>
+        )}
       </div>
     </div>
   );
