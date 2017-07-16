711e8635e9ba6c80df2340e45021f37290c32116
Aaron Endsley <aaronendsley@gmail.com>
2017-07-16 17:54:04 -0500

    created the menu button and styled it

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import navProfilePic from '../images/face.jpg';
-import './NavBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import navProfilePic from "../images/face.jpg";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div className="navBarContainer">
       <header>
-        <h1>
-          {'<Aaron Endsley/>'}
-        </h1>
         <div className="brand">
+          {" "}<h1>{"<Aaron Endsley/>"}</h1>
           <div className="imageFloat2">
             <div className="imgContainer2">
               <img src={navProfilePic} alt={`Aaron Endsley`} />
             </div>
           </div>
         </div>
+        <button className="menuButton" type="button">
+          &#9776;
+        </button>
       </header>
     </div>
   );
