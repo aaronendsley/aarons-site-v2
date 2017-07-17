3a13080cf2a0a44813693aa40ef5afdc659c3594
Aaron Endsley <aaronendsley@gmail.com>
2017-07-16 23:02:18 -0500

    wrote a function in the navbar component that will show and hide the navigation on button click

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import navProfilePic from "../images/face.jpg";
 import "./NavBar.css";
 
-const NavBar = () => {
-  return (
-    <div className="navBarContainer">
-      <header>
-        <div className="brand">
-          {" "}<h1>{"<Aaron Endsley/>"}</h1>
-          <div className="imageFloat2">
-            <div className="imgContainer2">
-              <img src={navProfilePic} alt={`Aaron Endsley`} />
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navState: false,
+      navClass: "navClose"
+    };
+    this.navToggle = this.navToggle.bind(this);
+  }
+  navToggle() {
+    console.log("click");
+    if (this.state.navState) {
+      this.setState(() => ({
+        navClass: "navClose",
+        navState: false
+      }));
+    } else {
+      this.setState(() => ({
+        navClass: "navOpen",
+        navState: true
+      }));
+    }
+  }
+  render() {
+    return (
+      <div className="navBarContainer">
+        <header>
+          <div className="brand">
+            {" "}<h1>{"<Aaron Endsley/>"}</h1>
+            <div className="imageFloat2">
+              <div className="imgContainer2">
+                <img src={navProfilePic} alt={`Aaron Endsley`} />
+              </div>
             </div>
           </div>
+          <button className="menuButton" type="button" onClick={this.navToggle}>
+            &#9776;
+          </button>
+        </header>
+        <div className={this.state.navClass}>
+          <p>ON</p>
         </div>
-        <button className="menuButton" type="button">
-          &#9776;
-        </button>
-      </header>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default NavBar;
