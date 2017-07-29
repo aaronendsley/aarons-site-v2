d954896e671b262690a2959a926e606d003b7b1d
Aaron Endsley <aaronendsley@gmail.com>
2017-07-29 11:49:52 -0500

    added the dependency react icons and used it to make each social button have that social networks icon

--- src/SocialBar/SocialBar.js
+++ src/SocialBar/SocialBar.js
@@ -1,23 +1,62 @@
-import React, { Component } from "react";
-import "./SocialBar.css";
-import { SocialMediaLinks } from "../data.json";
+import React, { Component } from 'react';
+import './SocialBar.css';
+import FaTwitterSquare from 'react-icons/lib/fa/twitter-square';
+import FaLinkedinSquare from 'react-icons/lib/fa/linkedin-square';
+import FaGithubSquare from 'react-icons/lib/fa/github-square';
+import { SocialMediaLinks } from '../data.json';
 
 class SocialBar extends Component {
   constructor(props) {
     super(props);
     this.social = SocialMediaLinks.social;
+    this.rGithub = this.rGithub.bind(this);
+    this.rLinkedIn = this.rLinkedIn.bind(this);
+    this.rTwitter = this.rTwitter.bind(this);
+    this.findAndReturn = this.findAndReturn.bind(this);
+  }
+
+  rGithub() {
+    return <FaGithubSquare />;
+  }
+  rLinkedIn() {
+    return <FaLinkedinSquare />;
+  }
+
+  rTwitter() {
+    return <FaTwitterSquare />;
+  }
+  findAndReturn(itemName) {
+    let itemToReturn;
+    switch (itemName) {
+      case 'Linked-In':
+        itemToReturn = this.rLinkedIn;
+        break;
+      case 'Github':
+        itemToReturn = this.rGithub;
+        break;
+      case 'Twitter':
+        itemToReturn = this.rTwitter;
+        break;
+      default:
+        console.error(`No such Item as ${itemName} found`);
+    }
+    return itemToReturn;
   }
 
   render() {
     return (
       <div className="socialBarContainer">
-        {this.social.map(item =>
-          <a target="blank" href={item.siteLink} key={item.siteName}>
-            <button className="socialBarChild">
-              {item.siteName}
-            </button>
-          </a>
-        )}
+        {this.social.map(item => {
+          let renIcon = this.findAndReturn(item.siteName);
+
+          return (
+            <a target="blank" href={item.siteLink} key={item.siteName}>
+              <button className="socialBarChild">
+                {renIcon()}
+              </button>
+            </a>
+          );
+        })}
       </div>
     );
   }
